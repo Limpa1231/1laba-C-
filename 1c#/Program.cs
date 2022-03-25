18e3684be3df026ccd58e7812ed35335cbe13838
Limpa1231 <90981641+Limpa1231@users.noreply.github.com>
2022-03-25 21:07:09 +0300

    Add files via upload

---
+++ 1c#/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace lab1._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student1 = new Student() { Age = 21, Id = 143, GroupNumber = 24, Name = "Симсон"};
+            Student student2 = new Student() { Age = 27, Id = 325, GroupNumber = 32, Name = "Леви"};
+
+            student1.Add(student2);
+            student2.AddThree(student1, student2);
+            Console.WriteLine("Student1:");
+            Console.WriteLine($"Id = {student1.Id}, Age = {student1.Age}, Name = {student1.Name}, GroupNumber = {student1.GroupNumber}");
+            Console.WriteLine("Student2:");
+            Console.WriteLine($"Id = {student2.Id}, Age = {student2.Age}, Name = {student2.Name}, GroupNumber = {student2.GroupNumber}");
+        }
+    }
+}
---
+++ 1c#/Student.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab1._2
+{
+    internal class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int GroupNumber { get; set; }
+
+        public void Add(Student s)
+        {
+            Id += s.Id;
+            Name += s.Name;
+            Age += s.Age;
+            GroupNumber += s.GroupNumber;
+        }
+        public void AddThree(Student s1, Student s2)
+        {
+            Id += s1.Id;
+            Name += s1.Name;
+            Age += s1.Age;
+            GroupNumber += s1.GroupNumber;
+
+            Id += s2.Id;
+            Name += s2.Name;
+            Age += s2.Age;
+            GroupNumber += s2.GroupNumber;
+        }
+    }
+}
